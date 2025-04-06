5680b91ede59a139259ab4fe46002b637b65ef83
Ravimisra72 <ravimishra5706@gmail.com>
2025-04-07 00:31:57 +0530

    Fix mobile support and random color issue

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ function btnPress() {
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
     btn.addEventListener("click", btnPress);
+    btn.addEventListener("touchstart", btnPress);
 }
 
 function reset() {
